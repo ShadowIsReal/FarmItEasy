c149d11072c748b3292b173067a9336adf053b76
Shadow <91681532+ShadowIsReal@users.noreply.github.com>
2026-04-17 00:12:10 +0100

    issue fix

--- dependencies/modules/initializer.lua
+++ dependencies/modules/initializer.lua
@@ -4,10 +4,10 @@ function initializer.load_environment(required_module_list, repo : string)
     getgenv().interface = loadstring(game:HttpGet(repo .. "interface/library.lua"))()
 
     -- modules
-    for module_index in required_module_list do
+    for module_index=1, #required_module_list do
         local module_name = required_module_list[module_index]
 
-        getgenv()[module_name] = loadstring( repo .. "modules/" .. module_name .. ".lua")()
+        getgenv()[module_name] = loadstring(game:HttpGet(repo .. "modules/" .. module_name .. ".lua"))()
     end
 end
 
