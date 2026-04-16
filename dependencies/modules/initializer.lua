a1a6487bea84225f23984c070860fe47dd34d68e
Shadow <91681532+ShadowIsReal@users.noreply.github.com>
2026-04-16 22:58:01 +0100

    simplified loading

--- dependencies/modules/initializer.lua
+++ dependencies/modules/initializer.lua
@@ -1,11 +1,11 @@
 local initializer = {}
 
-function initializer.load_environment(load_data, repo : string)
+function initializer.load_environment(required_module_list, repo : string)
     getgenv().interface = loadstring(game:HttpGet(repo .. "interface/library.lua"))()
 
     -- modules
-    for module_index in load_data.module_list do
-        local module_name = load_data.module_list[module_index]
+    for module_index in required_module_list do
+        local module_name = required_module_list[module_index]
 
         getgenv()[module_name] = loadstring( repo .. "modules/" .. module_name .. ".lua")()
     end
