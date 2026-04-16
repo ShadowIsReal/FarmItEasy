d28305914f16343221b567f86a172a4a67eaaba7
Shadow <91681532+ShadowIsReal@users.noreply.github.com>
2026-04-16 21:51:11 +0100

    fixed repo pathing

--- dependencies/modules/initializer.lua
+++ dependencies/modules/initializer.lua
@@ -11,13 +11,13 @@ function initializer.load_environment(load_data, repo : string)
             getgenv()[module_name] = loadfile("FarmItEasy/dependencies/modules/" .. module_name .. ".lua")()
         end
     else
-        getgenv().interface = loadstring(game:HttpGet(repo .. "/dependencies/interface/library.lua"))()
+        getgenv().interface = loadstring(game:HttpGet(repo .. "interface/library.lua"))()
 
         -- modules
         for module_index in load_data.module_list do
             local module_name = load_data.module_list[module_index]
 
-            getgenv()[module_name] = loadstring( repo .. "/dependencies/modules/" .. module_name .. ".lua")()
+            getgenv()[module_name] = loadstring( repo .. "modules/" .. module_name .. ".lua")()
         end
     end
 end
