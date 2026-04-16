594f3b6f0f395155a1ad8d6c94a653e96fd42348
Shadow <91681532+ShadowIsReal@users.noreply.github.com>
2026-04-16 22:56:25 +0100

    removed local loading for security purposes ( will be added back in the future )

--- dependencies/modules/initializer.lua
+++ dependencies/modules/initializer.lua
@@ -1,24 +1,13 @@
 local initializer = {}
 
 function initializer.load_environment(load_data, repo : string)
-    if load_data.load_locally then
-        getgenv().interface = loadfile("FarmItEasy/dependencies/interface/library.lua")()
+    getgenv().interface = loadstring(game:HttpGet(repo .. "interface/library.lua"))()
 
-       -- modules
-        for module_index in load_data.module_list do
-            local module_name = load_data.module_list[module_index]
+    -- modules
+    for module_index in load_data.module_list do
+        local module_name = load_data.module_list[module_index]
 
-            getgenv()[module_name] = loadfile("FarmItEasy/dependencies/modules/" .. module_name .. ".lua")()
-        end
-    else
-        getgenv().interface = loadstring(game:HttpGet(repo .. "interface/library.lua"))()
-
-        -- modules
-        for module_index in load_data.module_list do
-            local module_name = load_data.module_list[module_index]
-
-            getgenv()[module_name] = loadstring( repo .. "modules/" .. module_name .. ".lua")()
-        end
+        getgenv()[module_name] = loadstring( repo .. "modules/" .. module_name .. ".lua")()
     end
 end
 
